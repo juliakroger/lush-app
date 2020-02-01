3f861daa96518a884fd705747821b588635b2121
Júlia Kröger <julia.kroger@gmail.com>
2020-02-01 19:22:52 -0300

    feat: adding footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Icon, SubscriptionButton } from '..'
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.topFooter}>
+        <div className={styles.allLinks}>
+          <div className={styles.list}>
+            <span className={styles.title}>Customer Care</span>
+            <a href='/'>Contact</a>
+            <a href='/'>Shipping</a>
+            <a href='/'>Returns</a>
+            <a href='/'>FAQ</a>
+            <a href='/'>Policies</a>
+          </div>
+          <div className={styles.list}>
+            <span className={styles.title}>Discover</span>
+            <a href='/'>Our Story</a>
+          </div>
+        </div>
+
+        <div className={styles.newsSubscription}>
+          <span className={styles.title}>Get the latest Lush News!</span>
+          <SubscriptionButton />
+        </div>
+      </div>
+
+      <div className={styles.bottomFooter}>
+        <div className={styles.socialIcons}>
+          <div className={styles.icon}>
+            <Icon type='twitter' />
+          </div>
+          {/* facebook */}
+          <div className={styles.icon}>
+            <Icon type='github' />
+          </div>
+          <div className={styles.icon}>
+            <Icon type='instagram' />
+          </div>
+        </div>
+
+        <div className={styles.developedBy}>
+          <span>
+            Developed with <Icon type='heart' /> by Júlia Kroger.
+          </span>
+        </div>
+
+        <div className={styles.company}>
+          <span>©2020 - Lush Brazil used for portfolio.</span>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
