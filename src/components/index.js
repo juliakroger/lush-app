8e293866699ad655bdf6752ae5a930f5d06c5b55
Júlia Kröger <julia.kroger@gmail.com>
2020-03-01 19:33:10 -0300

    fix: change footer to container folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Icon } from './Icon/Icon'
-export { default as Footer } from './Footer/Footer'
 export { default as SubscriptionButton } from './SubscriptionButton/SubscriptionButton'
 export { default as PaymentsAccepted } from './PaymentsAccepted/PaymentsAccepted'
--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, SubscriptionButton, PaymentsAccepted } from '..'
+import { Icon, SubscriptionButton, PaymentsAccepted } from '../../components'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
