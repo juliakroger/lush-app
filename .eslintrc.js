fb726d01b3bea5a04dccae0546e055cac1baea87
Júlia Kröger <julia.kroger@gmail.com>
2020-03-05 10:34:42 -0300

    fix: fix eslint react setting warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,19 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react'],
-  rules: {}
+  rules: {},
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect',
+      flowVersion: '0.53'
+    },
+    propWrapperFunctions: [
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' }
+    ],
+    linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }]
+  }
 }
