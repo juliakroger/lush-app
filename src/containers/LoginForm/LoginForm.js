a830b128d81b30e54d97b86b6038d122b3b57c7f
Júlia Kröger <julia.kroger@gmail.com>
2020-03-14 10:20:34 -0300

    feat: login form container

---
+++ src/containers/LoginForm/LoginForm.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Input, Button } from '../../components'
+import styles from './LoginForm.module.scss'
+
+const LoginForm = ({ email, setEmail, password, setPassword, submit }) => {
+  return (
+    <div className={styles.loginForm}>
+      <h1>Sign In</h1>
+      <Input
+        placeholder='amy@example.com'
+        value={email}
+        valueChange={value => setEmail(value)}
+        id='#signin-email'
+        label='Email'
+        type='email'
+      />
+      <Input
+        value={password}
+        valueChange={value => setPassword(value)}
+        id='#signin-password'
+        label='Password'
+        secondaryLabel='Forgot password?'
+        type='password'
+        clickSecondary={() => {}}
+      />
+
+      <Button
+        text='sign in'
+        type={!email || !password ? 'buttonLight' : 'buttonMint'}
+        id='#login-button'
+        clickFunction={submit}
+      />
+      <span>
+        Don&apos;t have an account? Create one <a href='/'>here</a>.
+      </span>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired
+}
+
+export default LoginForm
